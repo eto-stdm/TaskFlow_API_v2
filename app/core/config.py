a11e9e4a4e5541b7feff556cd5e5bfa5f3981184
eto-stdm <potatocuteuwu@gmail.com>
2026-07-16 12:50:19 +0300

    обновлён конфиг, работающий с .env

--- app/core/config.py
+++ app/core/config.py
@@ -3,4 +3,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DB_URL: str
 
+    class Config:
+        env_file = "app/.env"
+        env_file_encoding = "utf-8"
+
 settings = Settings()
